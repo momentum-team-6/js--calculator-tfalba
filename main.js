d4826319c4267fc04e5592328391dfa53d22802c
Tracy Falba <tfalba@mac.com>
2020-11-12 17:19:28 -0500

    first working version

---
+++ main.js
@@ -0,0 +1,44 @@
+const display = document.querySelector('.rectangle')
+const container = document.querySelector('.container')
+
+let input=""
+let output=""
+container.addEventListener('click', function (event) {
+    if (event.target.classList.contains('keep-going')) {
+    console.log(event)
+    console.log(event.target)
+    input=input+event.target.innerText
+    // display.innerHTML = event.target.innerText
+    if (input.length>3) {
+        display.classList.add('highlight')
+        display.innerHTML = input
+    }
+    else if (input.length>2) {
+        display.classList.add('highlight2')
+        display.innerHTML = input
+    }
+    else {
+        display.innerHTML = input
+}}
+else if (event.target.classList.contains('equals')) {
+    output=eval(input)
+    display.innerHTML = output
+}
+else if (event.target.classList.contains('boxc')) {
+    input=""
+    display.innerHTML =""
+    display.classList.remove('highlight')
+
+}
+
+})
+
+
+
+// document.addEventListener('click',event => {
+//     console.log(event.target)
+//     event.target.classList.add('highlight')
+// })
+
+    // event.target.classList.add('highlight')
+
